71b24e530bfc65f53238e3385d2c287e76c2d1ab
Hytorke <Hytorke@gmail.com>
2021-08-21 22:42:15 +0200

    maj index.js

--- index.js
+++ index.js
@@ -5,4 +5,13 @@ const config = require("./config.json");
 const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] })
 client.login(config.token)
 
-//Fist code
+//Github and basics commands
+
+client.on('message', msg => {
+    if (msg.content == 'github' ){
+        msg.reply("My creator github is : https://github.com/LeRoiBlazzer/Hytorke-s-Bot.git")
+        console.log("Github sent")
+    }
+});
+
+//send a image
