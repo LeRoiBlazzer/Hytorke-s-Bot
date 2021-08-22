7ad5edf34b8116976bb4124df18bab1ca0dd5bcc
Hytorke <Hytorke@gmail.com>
2021-08-22 18:06:53 +0200

    not work

--- index.js
+++ index.js
@@ -2,16 +2,29 @@
 
 const Discord = require("discord.js");
 const config = require("./config.json");
+const { MessageButton } = require("discord-buttons");
 const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] })
 client.login(config.token)
 
 //Github and basics commands
 
 client.on('message', msg => {
-    if (msg.content == 'github' ){
+    if (msg.content == '??github' ){
         msg.reply("My creator github is : https://github.com/LeRoiBlazzer/Hytorke-s-Bot.git")
         console.log("Github sent")
     }
 });
 
-//send a image
+//send a button
+
+const buttontuto = new MessageButton()
+.setStyle("red")
+.setLabel("Test")
+.setID("buttontuto")
+
+client.on('message', msg => {
+    if (msg.content == "??bouton"){
+        message.reply("test de bouton", buttontuto)
+        console.log("Buttontuto send")
+    }
+})
