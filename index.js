4639d0e8a9a5146eaf384d23c3725009d2d90b08
Hytorke <Hytorke@gmail.com>
2021-08-23 18:00:56 +0200

    maj v 1.1

--- index.js
+++ index.js
@@ -2,29 +2,27 @@
 
 const Discord = require("discord.js");
 const config = require("./config.json");
-const { MessageButton } = require("discord-buttons");
 const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] })
+const prefix = "h?" 
 client.login(config.token)
+console.log('Bot is on');
 
 //Github and basics commands
 
 client.on('message', msg => {
-    if (msg.content == '??github' ){
+    if (msg.content == 'h?github' ){
         msg.reply("My creator github is : https://github.com/LeRoiBlazzer/Hytorke-s-Bot.git")
         console.log("Github sent")
     }
 });
 
-//send a button
-
-const buttontuto = new MessageButton()
-.setStyle("red")
-.setLabel("Test")
-.setID("buttontuto")
+//Split
 
 client.on('message', msg => {
-    if (msg.content == "??bouton"){
-        message.reply("test de bouton", buttontuto)
-        console.log("Buttontuto send")
-    }
-})
+    if (msg.content.startsWith(prefix)){
+        console.log('message send !');
+    } else {
+        msg.channel.send("This is the good prefix, but the command is wrong. Retry :smirk:")
+        console.log('Wrong command message send');
+    };
+});
